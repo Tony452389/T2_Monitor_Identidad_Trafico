2c8c3908da1d727c7b7273e8e479c607616fb3d3
SergioSR <94080707+AlphaOmegaSR@users.noreply.github.com>
2026-03-12 11:31:51 -0600

    Update Identidad.h

--- include/Identidad.h
+++ include/Identidad.h
@@ -1,3 +1,10 @@
-#pragma once
+#ifndef MONITOR_RED_H
+#define MONITOR_RED_H
 
-void iniciarIdentidad();
+#include <string>
+
+std::string getMAC(const std::string& iface);
+std::string getIP(const std::string& iface);
+void logEvent(const std::string& msg);
+
+#endif
