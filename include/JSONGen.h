90f3af94cd10fb204fe3ac6ce4540d7aadb354e4
Ricardo Estrada <112839745+ricardo-hervey@users.noreply.github.com>
2026-03-12 17:08:07 -0600

    Add JSONGen header

--- include/JSONGen.h
+++ include/JSONGen.h
@@ -1,3 +1,16 @@
-#pragma once
+#ifndef JSONGEN_H
+#define JSONGEN_H
 
-void iniciarJSON();
+#include <nlohmann/json.hpp>
+#include <vector>
+#include <string>
+
+class JSONGen {
+public:
+    static void addEvent(const nlohmann::json& event);
+    static void saveToFile(const std::string& filename);
+private:
+    static std::vector<nlohmann::json> events;
+};
+
+#endif
